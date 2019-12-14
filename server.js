12995048d0df98e0fb6734a3986f16b8d1e12748
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-12-14 17:28:45 -0500

    database disconnected, trying to debug... 'MONGODB_URI is not defined'...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -26,23 +27,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-
-
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/scraperController.js");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/macWorldDB", { useNewUrlParser: true });
+// mongoose.connect(MONGODB_URI);
